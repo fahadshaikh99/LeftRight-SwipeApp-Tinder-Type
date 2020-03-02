ee0f9e66e0ce6d93467051e472ec7246e62a066c
Fahad Shaikh <fahadnaeemsheikh@outlook.com>
2020-03-03 03:52:21 +0500

    first commit

---
+++ src/Deck.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import { Animated, PanResponder, Dimensions } from 'react-native';
+import { View } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+class Deck extends Component {
+
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSiwpeLeft: () => {}
+    }
+
+    constructor(props) {
+        super(props);
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+
+                position.setValue({ x: gesture.dx, y: gesture.dy });
+
+
+            },
+            onPanResponderRelease: (event, gesture) => { 
+
+                if ( gesture.dx > SWIPE_THRESHOLD ) {
+                    this.forceSwipe('right');
+                }
+                else if (gesture.dx < -SWIPE_THRESHOLD ) {
+                    this.forceSwipe('left');
+                }
+                else {
+
+                this.resetPosition();
+                }
+            }
+        });
+        this.state = { panResponder, position, index: 0 };
+    }
+
+    getCardStyle() {
+        const { position } = this.state
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5 , 0 , SCREEN_WIDTH * 1.5 ],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+        return {
+            ...this.state.position.getLayout(),
+            transform: [{ rotate }] 
+        };
+    }
+
+    forceSwipe(direction) {
+
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH; 
+
+        Animated.timing(this.state.position, {
+       
+            toValue: { x , y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction) {
+        const { onSiwpeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.props.index]
+        this.state.position.setValue({ x: 0, y: 0});
+        this.setState({ index: this.state.index + 1 });
+        direction === 'right' ? onSwipeRight(item) : onSiwpeLeft(item);
+    }
+
+    
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
+
+        }).start();
+
+    }
+
+    renderCard() {
+        if( this.state.index >= this.props.data.length) {
+            return this.props.renderNoMoreCards();
+        }
+        return this.props.data.map((item, i) => {
+            if (i < this.state.index) { return null; }
+            if (i === this.state.index) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={[this.getCardStyle(), styles.cardStyles ]}
+                        {...this.state.panResponder.panHandlers}>
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+
+                );
+            }
+
+            return ( 
+                <View key={item.id} style={styles.cardStyle}>
+                {this.props.renderCard(item)}
+                </View>
+        );
+        }).reverse();
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderCard()}
+            </View>
+        );
+    }
+}
+
+const styles = {
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH,
+        zIndex: 1
+    }
+};
+
+
+export default Deck;
